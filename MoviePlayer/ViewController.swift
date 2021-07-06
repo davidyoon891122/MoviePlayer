8d29f64388d7a985d4bb4660d121b44e66b611ac
davidyoon <qqpo12@naver.com>
2021-07-06 20:38:45 +0900

    Initial Commit

---
+++ MoviePlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviePlayer
+//
+//  Created by David Yoon on 2021/07/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
